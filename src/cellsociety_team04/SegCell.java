d4bb14a75909771e6bf8cd876a6f9cbca269af44
Aaron <anp36@duke.edu>
2017-09-22 17:29:55 -0400

    Debugged Segregation, reworked cell movement operation

--- src/cellsociety_team04/SegCell.java
+++ src/cellsociety_team04/SegCell.java
@@ -9,9 +9,6 @@ import javafx.scene.paint.Color;
  */
 public class SegCell extends Cell{
 
-	private boolean fillRed;		//marks an empty SegCell to which a Red SegCell will move
-	private boolean fillBlue;		//marks an empty SegCell to which a Blue SegCell will move
-	
 	public SegCell(String s) {
 		super(s);
 	}
@@ -24,25 +21,6 @@ public class SegCell extends Cell{
 		}
 		else if(s.equals("Blue")) {
 			color = Color.BLUE;
-		}
-	}
-	
-	public boolean getFillRed() {
-		return fillRed;
-	}
-	
-	public boolean getFillBlue() {
-		return fillBlue;
-	}
-	
-	public void markFill(String fill) {
-		if(status.equals("Empty")) {
-			if(fill.equals("Red")) {
-				fillRed = true;
-			}
-			else if(fill.equals("Blue")) {
-				fillBlue = true;
-			}
 		}
 	}
 	
