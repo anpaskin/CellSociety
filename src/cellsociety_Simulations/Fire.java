89c883e01750fc0290c104a13ec17e88423ca274
Kelly Zhang <kz49@duke.edu>
2017-09-30 18:55:47 -0400

    setting up fire probcatch slider

--- src/cellsociety_Simulations/Fire.java
+++ src/cellsociety_Simulations/Fire.java
@@ -133,6 +133,9 @@ public class Fire extends CellManager {
 		return false;
 	}
 	
+	private void setPCatch(double newPCatch) {
+		pCatch = newPCatch;
+	}
 	
 	
 }
--- src/cellsociety_UIUX/FireWindow.java
+++ src/cellsociety_UIUX/FireWindow.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import cellsociety_Simulations.CellManager;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
@@ -11,11 +13,26 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class FireWindow extends SimulationWindow {
-	private Slider probCatch;
+	private Slider probCatch = new Slider();
 	
 	public FireWindow(Stage s, CellManager sim) {
 		super(s, sim);
 		controls.add(probCatch);
 		addExtraSliders(probCatch, 0.0, 1.0, 0.5, 0.25, 0.5);
+		updateExtras(probCatch);
+	}
+	
+	protected void sliderDrag() {
+		probCatch.setOnMouseReleased(e -> {
+			running = false;
+			playButton.setGraphic(getImageView(RESET_PNG));
+			updateExtras(probCatch);
+			System.out.println("press reset");
+		});
+	}
+	
+	protected void updateExtras(Slider mySlider) {
+		//simType = new Fire(mySlider.getValue());
+		System.out.println(mySlider.getValue());
 	}
 }
